757618c08c2fbc834cbcd090e854b950c06b1f91
woasishen <76015064@qq.com>
2017-10-13 00:02:36 +0800

    fff

---
+++ Assets/_Script/BabySchedule/Panels/BaseVerticalScrollView.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using PathologicalGames;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BabySchedule.Panels
+{
+    public abstract class BaseVerticalScrollView : MonoBehaviourBase
+    {
+        protected abstract string CellPath { get; }
+
+        protected abstract int CellCount { get; }
+        protected abstract void UpdateItem(Transform item, int index);
+
+        protected ScrollRect ScrollRect;
+
+        private float _cellHight;
+        private int _usingCellCount;
+        
+
+        protected override void Awake()
+        {
+            base.Awake();
+            Spawns.Instance.CheckAndPrepareSpawnPool(Spawns.Instance.ViewCellPool, CellPath);
+
+            ScrollRect = GetComponent<ScrollRect>();
+            ScrollRect.verticalScrollbar.onValueChanged.AddListener(OnScrollRectScrolled);
+
+            _cellHight = ScrollRect.content.rect.height;
+            _usingCellCount = (int)Math.Ceiling(ScrollRect.viewport.rect.height / _cellHight) + 1;
+        }
+
+        public void ReloadData(bool keepPosition = false)
+        {
+            if (!keepPosition)
+            {
+                ScrollRect.content.anchoredPosition = new Vector2(0, 0);
+            }
+            
+
+            ScrollRect.content.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, CellCount * _cellHight);
+
+            var startIndex = (int)(ScrollRect.content.anchoredPosition.y / _cellHight);
+
+            var endIndex = Math.Min(startIndex + _usingCellCount, CellCount);
+            for (var i = startIndex; i < endIndex; i++)
+            {
+                var tempItem = Spawns.Instance.ViewCellPool.Spawn(CellPath);
+                tempItem.SetParent(ScrollRect.content);
+                tempItem.localPosition = new Vector3(0, - i * _cellHight);
+                UpdateItem(tempItem, i);
+            }
+        }
+
+        private void OnScrollRectScrolled(float arg)
+        {
+            //if (_cellHight - _usingCells.First.Value.localPosition.y < ScrollRect.content.anchoredPosition.y)
+            //{
+            //    Spawns.Instance.ViewCellPool.Despawn(_usingCells.First.Value.transform);
+
+            //    _usingCells.RemoveFirst();
+            //}
+        }
+    }
+}
---
+++ Assets/_Script/BabySchedule/Panels/Main/MainVerticalScrollView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using PathologicalGames;
+using TcpConnect;
+using UnityEngine;
+
+namespace BabySchedule.Panels.Main
+{
+    public class MainVerticalScrollView : BaseVerticalScrollView
+    {
+        protected override string CellPath => @"Prefabs/Panels/VIews/DiaperViewItems/Cell";
+
+        protected override int CellCount
+        {
+            get { return 20; }
+        }
+
+        protected override void UpdateItem(Transform item, int index)
+        {
+            
+        }
+
+        public void SetData(int dropDownValue)
+        {
+            
+        }
+    }
+}
