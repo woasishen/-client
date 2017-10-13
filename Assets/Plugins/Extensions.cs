0a31c2a7e441cdd3ac76169648073ddef34767c6
woasishen <76015064@qq.com>
2017-10-13 18:22:07 +0800

    fff

---
+++ Assets/Plugins/Extensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class Extensions
+{
+}
+
+public static class EnumHelper
+{
+    public static bool TryParse<TEnum>(String value, out TEnum result) where TEnum : struct
+    {
+        try
+        {
+            result = (TEnum)Enum.Parse(typeof(TEnum), value);
+            return true;
+        }
+        catch
+        {
+            result = default(TEnum);
+            return false;
+        }
+    }
+}
